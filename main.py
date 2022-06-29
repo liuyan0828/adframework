d3c5f5b55a28b36fe73ecaebcb3195e997b34841
yanliu218994 <yanliu218994@sohu-inc.com>
2022-06-29 18:07:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-
 import pytest
 import os
 
 if __name__ == '__main__':
     # s输出打印信息，v输出详细信息。不指定则运行所有
-    args = ['-s', '-q', 'TestCase/Ad', '--alluredir', 'Report/xml_report', '-reruns=4', '-reruns-delay=4']
+    args = ['-s', '-q', 'TestCase/Ad', '--alluredir', 'Report/xml_report', '--reruns=4', '--reruns-delay=4']
     pytest.main(args)
 
     os.system('allure generate %s -o %s --clean' % ('Report/xml_report', 'Report/html_report'))
