a1c493759d3a3ebeb3a1348ec96f1f92b628fe07
Guo WeiHua(智能媒体研发中心) <weihuaguo@sohu-inc.com>
2023-05-08 15:15:11 +0800

    test

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ import os
 import sys
 
 
-curPath=os.path.abspath(os.path.dirname(__file__))
-rootPath=os.path.split(curPath)[0]
-sys.path.append(rootPath)
 
 if __name__ == '__main__':
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
     # s输出打印信息，v输出详细信息。不指定则运行所有
     #args = ['-s', '-q', 'TestCase/Ad', '--alluredir', 'Report/xml_report', '--reruns=4', '--reruns-delay=4']
     args = ['-s', '-q', 'TestCase/Ad', '--alluredir', 'Report/xml_report']
