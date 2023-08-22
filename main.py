8ea6baae5f853e5a41bcd0143e631eacc00dab04
yanliu218994 <yanliu218994@sohu-inc.com>
2023-08-22 15:44:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
     # s输出打印信息，v输出详细信息。不指定则运行所有
-    args = ['-s', '-q', 'TestCase/Ad', '--clean-alluredir', 'Report/xml_report', '--reruns=4', '--reruns-delay=4']
+    args = ['-s', '-q', 'TestCase/Ad', '--clean-alluredir', 'Report/xml_report']
     pytest.main(args)
 
     os.system('allure generate %s -o %s --clean' % ('Report/xml_report', 'Report/html_report'))
