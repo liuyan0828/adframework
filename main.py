b1d3693c5a74d3a6f0282277a4c815e493d60226
yanliu218994 <yanliu218994@sohu-inc.com>
2023-08-22 16:17:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ import sys
 
 if __name__ == '__main__':
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    
+    for i in os.listdir('Report/xml_report'):
+        #   模板文件都是json格式的
+        if 'json' in i:
+            os.remove(f'report/xml_report/{i}')
 
     # s输出打印信息，v输出详细信息。不指定则运行所有
     args = ['-s', '-q', 'TestCase/Ad', '--clean-alluredir', 'Report/xml_report']
