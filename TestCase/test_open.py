55d98e95cc9c87dde130bd152b28dd431da780ca
yanliu218994 <yanliu218994@sohu-inc.com>
2024-01-25 17:12:17 +0800

    test allure-report

--- TestCase/test_open.py
+++ TestCase/test_open.py
@@ -51,7 +51,7 @@ class Test_Ad_Open():
     @pytest.mark.parametrize("case_data", case_dict[0], ids=case_dict[1])
     @pytest.mark.xfail
     def test_ad_res(self, case_data, api_response):
-        allure.dynamic.title(f"{case_data}")
+        allure.title(f"{case_data}")
         with allure.step("校验是否为空广告"):
             adtype = jsonpath.jsonpath(api_response, '$..adtype')[0]
         assert adtype != -1, "请检查投放，当前广告位返回的是空广告"
@@ -65,7 +65,7 @@ class Test_Ad_Open():
         :param api_response: 请求返回数据
         :return:
         """
-        allure.dynamic.title(f"{case_data}")
+        allure.title(f"{case_data}")
         payload = case_data['payload']
         mango_conf = GetAdConf(payload)
         template = mango_conf.get_adtemplate()
@@ -120,7 +120,7 @@ class Test_Ad_Open():
     @allure.story("校验请求返回：1、返回广告的排期包id是否为基准广告id 2、如果是对返回进行完全校验")
     @pytest.mark.parametrize("case_data", case_dict[0], ids=case_dict[1])
     def test_res_complete_check(self, case_data, api_response):
-        allure.title(f"{case_data}")
+        allure.title(f"{case_data['title']}")
         expected_request = case_data['check']['expected_request']
         if isinstance(expected_request, str):
             _path = PATH + '/' + case_data['info']
