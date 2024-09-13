b02b4e98bbf150101e0b2733c8ce2417926a354c
yanliu218994 <yanliu218994@sohu-inc.com>
2024-09-13 17:19:58 +0800

    add case

--- TestCase/test_mp.py
+++ TestCase/test_mp.py
@@ -38,20 +38,3 @@ class Test_Ad_Mp():
         check_lineid(api_response, expected_request)
         check_returned_data(api_response, expected_request)
 
-    @allure.story("校验xml是否能返回：1、校验返回ad是否与基准一致 2、完全校验")
-    @pytest.mark.flaky(reruns=10, reruns_delay=1)
-    @pytest.mark.parametrize("case_data", case_dict[0], ids=case_dict[1])
-    def test_ad_xml(self, case_data):
-        allure.title(f"{case_data['title']}")
-        # prot=vast则返回xml
-        case_data['parameter']['prot'] = 'vast'
-        res = check_code(case_data)
-        res_data = RequestHandler.decode_xml_to_dict(res.content)
-        assert res_data != {}
-        # xml存放路径
-        path = PATH + '/' + case_data['info'] + '/' + case_data['check']['expected_xml']
-        base_xml = CompareXml.get_root(path)
-        expected_request = CompareXml.get_all_elements(base_xml)
-        root = ET.XML(res_data)
-        api_response = CompareXml.get_all_elements(root)
-        check_xml_res(api_response, expected_request)
